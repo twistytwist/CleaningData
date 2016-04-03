96b847114d8e1db9c0e53789fae4898d41f2029f
twistytwist <mhowe@outlook.com>
2016-04-03 09:15:16 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,22 @@
-
-run_analysis <- function(x_test, x_train) {  
+#Takes wearable data copied locally from here (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) 
+#and produces a tidy data set with the test and training data combined. 
+#It creates descriptive column names, replaces labels for activity ids and associates the right subject with 
+#the right data. Then, it produces a second tidy data set that contains the mean over each subject for all 
+#the values.
+#To use this function the files in the above zip must be stored locally in the working directory with the 
+#same folder structure defined in the zip.  
+run_analysis <- function() {  
   #we are going to use data.table...make sure it is loaded
   library(data.table) 
   
   #load data from test files    
   subject_test = read.table("UCI HAR Dataset/test/subject_test.txt") 
-#  x_test = read.table("UCI HAR Dataset/test/X_test.txt") 
+  x_test = read.table("UCI HAR Dataset/test/X_test.txt") 
   y_test = read.table("UCI HAR Dataset/test/Y_test.txt") 
   
   #load data from training files
   subject_train = read.table("UCI HAR Dataset/train/subject_train.txt") 
-#  x_train = read.table("UCI HAR Dataset/train/X_train.txt") 
+  x_train = read.table("UCI HAR Dataset/train/X_train.txt") 
   y_train = read.table("UCI HAR Dataset/train/Y_train.txt") 
   
   #load data from other files 
